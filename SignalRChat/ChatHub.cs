14becabc573dcb4b3e0e40da1fb3a148ea10cef0
anandgothe <10891466+anandgothe@users.noreply.github.com>
2019-03-19 17:40:52 +0530

    Add files via upload

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+namespace SignalRTimer
+{
+    public class ChatHub : Hub
+    {
+        StorageOnAzureTable sessionTimouts = new StorageOnAzureTable(); // you can use any storage here, even an inmemory dictionary will do. I'm using Azure Table storage for persistance, so that we don't lose client timer values if the server restarts.
+        
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message + Context.ConnectionId);
+            //Clients.we
+        }
+
+        public void RegisterClient(string sessionid)
+        {
+            var id = Context.ConnectionId;
+            Groups.Add(id, sessionid); //this will add the connected user to particular group
+
+            RefreshTimer(sessionid);
+        }
+
+        public void RefreshTimer(string sessionid)
+        {
+            bool isTimerOn = sessionTimouts.ContainsSession(sessionid);
+            var session = sessionTimouts.GetSession(sessionid);
+
+            if (isTimerOn)
+            {
+                int seconds = CalculateSeconds(session);
+                Clients.Caller.sessionStarted(sessionid, true, seconds, session.Message ?? string.Empty);
+            }
+            else
+            {
+                Clients.Caller.sessionStarted(sessionid, false);
+            }
+        }
+
+        public void StartTimer(string sessionid, string minutes, string message)
+        {
+            var whenEndsUtc = DateTime.UtcNow.AddMinutes(int.Parse(minutes));
+            sessionTimouts.SetSession(sessionid, whenEndsUtc, message);
+            BroadcastTimeLeft(sessionid);
+        }
+
+        private void BroadcastTimeLeft(string sessionid)
+        {
+            var session = sessionTimouts.GetSession(sessionid);
+            int seconds = CalculateSeconds(session);
+
+            string[] Exceptional = new string[0];
+            Clients.Group(sessionid, Exceptional).startCountdown(seconds,session.Message);
+        }
+
+        private int CalculateSeconds(SessionEntity session)
+        {
+            if (session != null)
+            {
+                var howMuchLeft = session.EndsWhenUtc.Subtract(DateTime.UtcNow);
+                var seconds = (int)howMuchLeft.TotalSeconds;
+                return seconds;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ SignalRChat/Storage.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Table; // Namespace for Table storage
+
+namespace SignalRTimer
+{
+    public class StorageOnAzureTable
+    {
+        CloudTable table;
+
+        public StorageOnAzureTable()
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            table = tableClient.GetTableReference("sessions");
+
+            // Create the table if it doesn't exist.
+            table.CreateIfNotExists();
+        }
+
+        internal bool ContainsSession(string sessionid)
+        {
+            return GetSession(sessionid)!=null;
+        }
+
+        internal void SetSession(string sessionid, DateTime endsWhenUtc, string message)
+        {
+            SessionEntity session = new SessionEntity(sessionid, message);
+            session.EndsWhenUtc = endsWhenUtc;
+
+            TableOperation insertOperation = TableOperation.InsertOrReplace(session);
+
+            // Execute the insert operation.
+            table.Execute(insertOperation);
+        }
+        
+        internal SessionEntity GetSession(string sessionid)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<SessionEntity>(sessionid,sessionid);
+
+            TableResult retrievedResult = table.Execute(retrieveOperation);
+
+            if (retrievedResult.Result != null)
+            {
+                var res = ((SessionEntity)retrievedResult.Result);
+
+                // 24 hours after countdown complete, let it go! remove the session
+                if (res.EndsWhenUtc.Subtract(DateTime.UtcNow).TotalHours < -24)
+                {
+                    return null;
+                }
+
+                return res;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public class SessionEntity : TableEntity
+    {
+        public SessionEntity(string name, string message)
+        {
+            this.PartitionKey = name;
+            this.RowKey = name;
+            this.Message = message;
+        }
+
+        public SessionEntity() { }
+
+        public DateTime EndsWhenUtc { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class StorageOnDictionary
+    {
+        public static Dictionary<string, DateTime> sessionTimouts = new Dictionary<string, DateTime>();
+
+        internal bool ContainsSession(string sessionid)
+        {
+            return sessionTimouts.ContainsKey(sessionid);
+        }
+
+        internal void SetSession(string sessionid, DateTime whenEndsUtc)
+        {
+            sessionTimouts[sessionid] = whenEndsUtc;
+        }
+
+        internal DateTime? GetSession(string sessionid)
+        {
+            return sessionTimouts[sessionid];
+        }
+    }
+
+}
